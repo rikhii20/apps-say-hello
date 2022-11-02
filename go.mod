8b2dc11ef5576e53e6700f12c9324942b40e8305
Rikhi <rikhisobari22@gmail.com>
2022-11-02 23:33:21 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/rikhii20/apps-say-hello
+
+go 1.19
+
+require github.com/rikhii20/appli-say-hello v1.0.0 // direct
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	greetings "github.com/rikhii20/appli-say-hello"
+)
+
+func main() {
+	fmt.Println(greetings.Hello())
+}
